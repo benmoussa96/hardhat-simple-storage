260270dcc8f583bab59ade2a49486767d8aeefac
benmoussa96 <ghaiethbmoussa@gmail.com>
2023-01-04 18:19:42 +0100

    accounts & block-number tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,17 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 import "@nomiclabs/hardhat-etherscan";
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// Tasks
+import "./tasks/accounts";
+import "./tasks/block-number";
 
+// Enviorment Variables
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PIVATE_KEY = process.env.PIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
+// You need to export an object to setup your config
+// Go to to https://hardhat.org/config learn more
+
+/**
+ * @type impot('hardhat/config').HardhatUserrConfig
+ */
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
