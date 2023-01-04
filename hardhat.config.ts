3c077e3f488953b16d94b04dfee0777bbe5b03f7
benmoussa96 <ghaiethbmoussa@gmail.com>
2023-01-04 17:44:32 +0100

    deploying to goerli and verifying on etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
+import "@nomiclabs/hardhat-etherscan";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PIVATE_KEY = process.env.PIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -13,6 +22,9 @@ const config: HardhatUserConfig = {
       accounts: [PIVATE_KEY!],
       chainId: 5,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
 
 async function main() {
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
@@ -7,6 +7,36 @@ async function main() {
   await simpleStorage.deployed();
 
   console.log(`SimpleStorage deployed to ${simpleStorage.address}`);
+
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value is ${currentValue}`);
+
+  const transactionRespons = await simpleStorage.store(6);
+  await transactionRespons.wait();
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Current value is ${updatedValue}`);
+}
+
+async function verify(contractAddress: string, args: any[]) {
+  console.log("Verifying contract...");
+
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error: any) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.log(error);
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
